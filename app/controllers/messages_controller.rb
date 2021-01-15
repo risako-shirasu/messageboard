9dbcced3bda357dfddaa687c66823b641f09fd9a
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-15 14:34:39 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,56 +1,56 @@
 class MessagesController < ApplicationController
-    def index
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
     @messages = Message.all
-    end
-    
-    def show
-    @message = Message.find(params[:id])
-    end
-    
-    def new
+  end
+
+  def show
+  end
+
+  def new
     @message = Message.new
-    end
-    
-    def create
+  end
+
+  def create
     @message = Message.new(message_params)
 
-        if @message.save
-          flash[:success] = 'Message が正常に投稿されました'
-          redirect_to @message
-        else
-          flash.now[:danger] = 'Message が投稿されませんでした'
-          render :new
-        end
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
     end
+  end
 
-    def edit
-      @message = Message.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
     end
+  end
 
-    def update
-      @message = Message.find(params[:id])
-  
-      if @message.update(message_params)
-        flash[:success] = 'Message は正常に更新されました'
-        redirect_to @message
-      else
-        flash.now[:danger] = 'Message は更新されませんでした'
-        render :edit
-      end
-    end
+  def destroy
+    @message.destroy
 
-    def destroy
-      @message = Message.find(params[:id])
-      @message.destroy
-  
-      flash[:success] = 'Message は正常に削除されました'
-      redirect_to messages_url
-    end
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+  end
 
-    private
+  private
 
-  # Strong Parameter
-    def message_params
-     params.require(:message).permit(:content)
-    end
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
