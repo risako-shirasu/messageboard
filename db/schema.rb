78e055f5a6d3870ca298d767fb0eab0c06594fe2
lisa.shiro <fortechacademy.risako1011@gmail.com>
2021-01-18 14:15:48 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_134918) do
+ActiveRecord::Schema.define(version: 2021_01_18_141424) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
